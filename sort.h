7f07101af973f6ca2cbf83605d1d0e0f368436bc
KatliKoma <katlegokoma1@gmail.com>
2024-02-26 22:46:56 +0200

    final

--- sort.h
+++ sort.h
@@ -25,7 +25,12 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void nodeSwap(listint_t **head, listint_t *currentNode, listint_t *nextNode);
-void quick_sort_recursion(int *array, int low, int high, size_t size);
-void quick_sort(int *array, size_t size);
-int lomuto_partition(int *array, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 #endif
